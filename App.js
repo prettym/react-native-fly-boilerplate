55b9d957c699bfe2d39bbc576b57884e072c5c37
Donghwa <moralmk@gmail.com>
2019-10-05 14:56:22 +0900

    Auth installation and anonymous sign-in for iOS only

--- App.js
+++ App.js
@@ -4,8 +4,24 @@ import {
   Text,
   StyleSheet,
 } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 export default class App extends React.Component {
+
+  async componentDidMount() {
+    try {
+      await auth().signInAnonymously();
+    } catch (e) {
+      switch (e.code) {
+        case 'auth/operation-not-allowed':
+          console.log('Enable anonymous in your firebase console.');
+          break;
+        default:
+          console.error(e);
+          break;
+      }
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
